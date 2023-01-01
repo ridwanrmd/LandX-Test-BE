30b0981eb371e0283e09d1ffc5e06452a88078d1
ridwan rmd <ridwan95rmd@gmail.com>
2023-01-02 01:03:15 +0700

    create new landxtest nest project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common/services';
+
+const PORT = process.env.PORT
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+
+  Logger.log(`API RUNNING at PORT ${PORT}`)
+}
+bootstrap();
