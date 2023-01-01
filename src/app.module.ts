c01cb61da621fc9c59a040a9f080fbb9283f638a
ridwan rmd <ridwan95rmd@gmail.com>
2023-01-02 01:12:29 +0700

    setup database connection with typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
+import 'dotenv/config'
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} = process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DB_HOST,
+      port: parseInt(DB_PORT),
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      entities: ['src/**/.*entity.ts', 'dist/**/*.entity.js'],
+      synchronize: true,
+      dropSchema: false,
+      logging: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
